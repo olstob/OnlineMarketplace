5d2bf45b97813b956c5e4a19e668144a491ab32c
Olivier St-Onge <olivier-alain.st-onge@polymtl.ca>
2019-01-16 21:59:32 -0500

    added the functions associated to the graphql queries and the type defs

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,22 @@
+import Product from "../../models/Product";
+
+export default {
+    Query: {
+        product: (root, args) => {
+            return new Promise((resolve, reject) => {
+                console.log(args);
+                Product.findOne(args).exec((err, res) => {
+                    err ? reject(err) : resolve(res);
+                });
+            });
+        },
+        products: (root, args) => {
+            return new Promise((resolve, reject) => {
+                const operator = args.in_stock ? { inventory_count: { $gt: 0 }} : {};
+                Product.find(operator).populate().exec((err, res) => {
+                    err ? reject(err) : resolve(res);
+                });
+            });
+        }
+    }
+}
---
+++ graphql/types/index.js
@@ -0,0 +1,6 @@
+import { mergeTypes } from "merge-graphql-schemas";
+import Product from "./Product/";
+
+const typeDefs = [Product];
+
+export default mergeTypes(typeDefs, { all: true });
