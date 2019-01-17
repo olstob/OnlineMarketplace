5040569a02e0320212bdedd33a25cfd60a75df47
Olivier St-Onge <olivier-alain.st-onge@polymtl.ca>
2019-01-16 19:14:03 -0500

    basic server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || "4000";
+const DB_URL = "mongodb://shopify_s2019_challenge:BJ6tr64XEUWvWGv@ds159184.mlab.com:59184/db_tea";
+
+// Connect to MongoDB with Mongoose.
+mongoose
+  .connect(
+    DB_URL,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use(
+    cors(),
+    bodyParser.json()
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
