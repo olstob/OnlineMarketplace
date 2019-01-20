0f0e20fb1b57b787eaf902be7c49bef0d5e12f3d
Olivier St-Onge <olivier-alain.st-onge@polymtl.ca>
2019-01-20 15:10:09 -0500

    Render README on home page

--- app/index.js
+++ app/index.js
@@ -4,9 +4,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import schema from "./graphql";
+import marked from "marked";
+import path from "path";
+import * as fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || "8000";
+
+const README = "/../README.md"
 
 // Using mLab to host a products database.
 // I normally would place the user and password in a untracked config file,
@@ -37,6 +42,12 @@ app.use(
       graphiql: true
     })
 );
+
+app.get('/', (req, res) => {
+  var path = __dirname + README;
+  var file = fs.readFileSync(path, 'utf8');
+  res.send(marked(file.toString()));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
